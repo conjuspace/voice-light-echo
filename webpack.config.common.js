cae6c56ab90ac13f658d9785d32bc0c529a091cd
Oleh Pshyk <pshyk.oleg@gmail.com>
2021-06-05 10:49:32 +0300

    add logo with font

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -48,16 +48,17 @@ module.exports = {
             },
           },
         ],
-      },
+      },      
       {
         test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [{
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]',
-            outputPath: 'fonts/',
-          },
-        }],
+        use: [
+          {
+            loader: 'file-loader',
+            options: {              
+              outputPath: 'static/fonts/',
+            },
+          }
+        ],
       },
     ],
   },
